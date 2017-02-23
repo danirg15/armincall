41970e8afafa541a9c12eaab4f169a8e1b77b1cf
danirg15 <dani@Danis-MacBook-Pro.local>
2017-02-23 10:18:37 +0100

    added new distributor and category in incomming popup

--- database/index.js
+++ database/index.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose');
 
 
 module.exports.connect = function(uri){
-    
-    mongoose.connect(uri, function(err) {
-        if (err) throw err;
-        console.log('Connected to DB: '+uri);
-    });
 
+    mongoose.connect(uri, function(err) {
+        if (err) throw err
+        console.log('Connected to DB: '+uri)
+    })
+    
 }
 
