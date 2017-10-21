bbbfb07181a9ab7c1a31da85ef7aafd6cf8874bc
Daniel Ramirez <dani@Danis-MacBook-Pro.local>
2017-10-21 10:46:47 +0200

    Fixed deprecared method in mongoose connection. Added map in workshop form

--- database/index.js
+++ database/index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 module.exports.connect = function(uri){
 
-    mongoose.connect(uri, function(err) {
+    mongoose.connect(uri, { 'useMongoClient': true }, function(err) {
         if (err) throw err
         console.log('Connected to DB: '+uri)
     })
