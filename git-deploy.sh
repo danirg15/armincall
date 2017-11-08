0c92f646e265ae4665c62e06388dd7a045b454b4
Daniel Ramirez <dani@Danis-MacBook-Pro.local>
2017-11-08 14:49:31 +0100

    updated git deploy

--- git-deploy.sh
+++ git-deploy.sh
@@ -2,18 +2,18 @@
 
 git checkout develop
 
-cd ./client && ng build --prod
+# cd ./client && ng build --prod
 
-if [[ $? != 0 ]]; then
-	echo "Error ng build --prod"
-	exit -1
-fi
+# if [[ $? != 0 ]]; then
+# 	echo "Error ng build --prod"
+# 	exit -1
+# fi
 
-cd ..
+# cd ..
 
 git add * && git add .*
 
-git commit -m "$1 & build"
+git commit -m "$1"
 
 git push origin develop
 
